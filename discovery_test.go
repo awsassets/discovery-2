a6b4105302a8562f539a456d61ad6417533af815
nothinux <nothinux@gmail.com>
2022-02-17 13:39:08 +0700

    fix test

--- discovery_test.go
+++ discovery_test.go
@@ -17,5 +17,15 @@ func TestDiscover(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	t.Log(d)
+	if d[0].Target != "ac110003.addr.dc1.consul" {
+		t.Fatalf("want ac110003.addr.dc1.consul, got %v", d[0].Target)
+	}
+
+	if d[0].Address.String() != "172.17.0.3" {
+		t.Fatalf("want 172.17.0.3, got %v", d[0].Address.String())
+	}
+
+	if d[0].Port != uint16(8200) {
+		t.Fatalf("want 8200, got %v", d[0].Port)
+	}
 }
